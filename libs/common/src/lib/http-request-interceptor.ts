2764f021fb48dc74654366989c7f93cf08848034
Zen Software <ZenSoftware@users.noreply.github.com>
2020-08-24 05:57:35 -0600

    format all

--- libs/common/src/lib/http-request-interceptor.ts
+++ libs/common/src/lib/http-request-interceptor.ts
@@ -1,5 +1,5 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
