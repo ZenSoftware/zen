99d61e109bb4e7eab81c6b2997ca344fa1a24f83
Zen Software <ZenSoftware@users.noreply.github.com>
2020-08-30 16:20:12 -0600

    apollo-angular HttpBatchLink

--- libs/graphql/src/lib/graphql.module.ts
+++ libs/graphql/src/lib/graphql.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, ApolloLink, InMemoryCache, split } from '@apollo/client/core';
 import { Environment } from '@zen/common';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpBatchLink } from 'apollo-angular/http';
 
-export function createApollo(httpLink: HttpBatchLink, env: Environment): ApolloClientOptions<any> {
-  return {
-    link: httpLink.create({ uri: env.appUrl.graphql }),
-    cache: new InMemoryCache(),
-  };
+export function createApollo(
+  httpBatchLink: HttpBatchLink,
+  env: Environment
+): ApolloClientOptions<any> {
+  const links: ApolloLink[] = [];
+
+  const batch_link = httpBatchLink.create({
+    uri: env.appUrl.graphql,
+    withCredentials: true,
+    batchMax: 500,
+  });
+
+  links.push(batch_link);
+
+  return { link: ApolloLink.from(links), cache: new InMemoryCache() };
 }
 
 @NgModule({
