9741207fb0d477e8c49e2471e3e4a6c217ffc879
Zen Software <ZenSoftware@users.noreply.github.com>
2020-08-30 14:38:08 -0600

    made prisma service scoped to requests

--- apps/api/src/app/prisma/prisma.service.ts
+++ apps/api/src/app/prisma/prisma.service.ts
@@ -1,10 +1,16 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit, Optional, Scope } from '@nestjs/common';
+import { PrismaDelete, onDeleteArgs } from '@paljs/plugins';
+import { PrismaClient, PrismaClientOptions } from '@prisma/client';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super();
+  constructor(@Optional() options?: PrismaClientOptions) {
+    super(options);
+  }
+
+  async onDelete(args: onDeleteArgs) {
+    const prismaDelete = new PrismaDelete(this);
+    await prismaDelete.onDelete(args);
   }
 
   async onModuleInit() {
