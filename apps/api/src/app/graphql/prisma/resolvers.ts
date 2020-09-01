43ad8cc7fd2e7cbb9948c6638a6d5e387ad05831
Zen Software <ZenSoftware@users.noreply.github.com>
2020-09-01 09:09:56 -0600

    format code

--- apps/api/src/app/graphql/prisma/resolvers.ts
+++ apps/api/src/app/graphql/prisma/resolvers.ts
@@ -1,5 +1,5 @@
-import Group from './Group/resolvers'
 import Comment from './Comment/resolvers'
+import Group from './Group/resolvers'
 import Post from './Post/resolvers'
 import User from './User/resolvers'
 export default [User, Post, Comment, Group]
--- apps/api/src/app/graphql/prisma/typeDefs.ts
+++ apps/api/src/app/graphql/prisma/typeDefs.ts
@@ -1,8 +1,9 @@
-import Group from './Group/typeDefs'
+import { sdlInputs } from '@paljs/plugins'
+import { mergeTypes } from 'merge-graphql-schemas'
+
 import Comment from './Comment/typeDefs'
+import Group from './Group/typeDefs'
 import Post from './Post/typeDefs'
 import User from './User/typeDefs'
-import { mergeTypes } from 'merge-graphql-schemas'
-import { sdlInputs } from '@paljs/plugins'
 
 export default mergeTypes([sdlInputs, User, Post, Comment, Group])
