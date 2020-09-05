ad8abb8666917988b6aebd6e669f3e0446dd4d56
Zen Software <ZenSoftware@users.noreply.github.com>
2020-09-04 21:52:30 -0600

    made helpers typesafe

--- libs/graphql/src/lib/tools/prisma-helpers.ts
+++ libs/graphql/src/lib/tools/prisma-helpers.ts
@@ -1,8 +1,11 @@
-export function selectOne(
-  item: object | number | string | null | undefined,
-  inputFieldName: string = 'id',
-  outputFieldName = inputFieldName
+export function selectOne<T>(
+  item: T | number | string | null | undefined,
+  inputFieldName?: keyof T,
+  outputFieldName?: string
 ) {
+  if (!inputFieldName) (<any>inputFieldName) = 'id';
+  if (!outputFieldName) (<any>outputFieldName) = inputFieldName;
+
   if (item !== undefined || item !== null) {
     const typeofItem = typeof item;
 
@@ -14,15 +17,15 @@ export function selectOne(
       (<any>item)[inputFieldName] !== ''
     ) {
       const obj: any = new Object();
-      obj[outputFieldName] = (<any>item)[inputFieldName];
+      obj[<any>outputFieldName] = (<any>item)[inputFieldName];
       return obj;
     } else if (typeofItem === 'number' && item !== -1) {
       const obj: any = new Object();
-      obj[outputFieldName] = item;
+      obj[<any>outputFieldName] = item;
       return obj;
     } else if (typeofItem === 'string' && item !== '') {
       const obj: any = new Object();
-      obj[outputFieldName] = item;
+      obj[<any>outputFieldName] = item;
       return obj;
     }
   }
@@ -30,11 +33,14 @@ export function selectOne(
   return undefined;
 }
 
-export function selectMany(
-  input: any[] | null | undefined,
-  inputFieldName: string = 'id',
-  outputFieldName = inputFieldName
+export function selectMany<T>(
+  input: Array<T | null | undefined> | null | undefined,
+  inputFieldName?: keyof T,
+  outputFieldName?: string
 ) {
+  if (!inputFieldName) (<any>inputFieldName) = 'id';
+  if (!outputFieldName) (<any>outputFieldName) = inputFieldName;
+
   if (input) {
     const items = (input as any[]).filter(x => x !== null && x !== undefined);
 
@@ -50,15 +56,15 @@ export function selectMany(
           (<any>item)[inputFieldName] !== ''
         ) {
           const obj: any = new Object();
-          obj[outputFieldName] = item[inputFieldName];
+          obj[<any>outputFieldName] = item[inputFieldName];
           accum.push(obj);
         } else if (typeofItem === 'number' && item !== -1) {
           const obj: any = new Object();
-          obj[outputFieldName] = item;
+          obj[<any>outputFieldName] = item;
           accum.push(obj);
         } else if (typeofItem === 'string' && item !== '') {
           const obj: any = new Object();
-          obj[outputFieldName] = item;
+          obj[<any>outputFieldName] = item;
           accum.push(obj);
         }
 
