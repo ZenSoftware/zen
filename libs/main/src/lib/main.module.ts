53684bc12ae0ed4e0e5f589ca4e41ca1a35da5b1
Zen Software <ZenSoftware@users.noreply.github.com>
2020-09-07 06:42:46 -0600

    setup portal theme

--- libs/main/src/lib/main.module.ts
+++ libs/main/src/lib/main.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { MainRoutingModule } from './main-routing.module';
 import { ZenMainComponent } from './zen-main/zen-main.component';
 
 @NgModule({
-  imports: [CommonModule, MainRoutingModule],
+  imports: [CommonModule, MainRoutingModule, MatSliderModule, MatButtonModule, MatDividerModule],
   declarations: [ZenMainComponent],
 })
 export class MainModule {}
