66b26d3dd2165c9a60f133da7b7b71da9de9abb9
Zen Software <ZenSoftware@users.noreply.github.com>
2020-09-06 22:08:30 -0600

    changed main example with case insensitive querying

--- libs/main/src/lib/zen-main/zen-main.component.ts
+++ libs/main/src/lib/zen-main/zen-main.component.ts
@@ -1,59 +1,24 @@
 import { Component } from '@angular/core';
-import {
-  FindManyUserGQL,
-  FindOneUserGQL,
-  QueryMode,
-  UpdateOneUserVariables,
-  selectMany,
-  selectOne,
-} from '@zen/graphql';
-import { map, shareReplay } from 'rxjs/operators';
+import { FindManyUserGQL, QueryMode } from '@zen/graphql';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'zen-main',
   templateUrl: 'zen-main.component.html',
 })
 export class ZenMainComponent {
-  constructor(private findOneUserGQL: FindOneUserGQL, private findManyUserGQL: FindManyUserGQL) {}
-
-  userInput: UpdateOneUserVariables['data'] = {
-    comments: { delete: selectMany([{ id: 1, example: '' }]) },
-    posts: { set: selectMany([1, 2, 3]) },
-    group: { connect: selectOne({ id: 1 }) },
-  };
-
-  user$ = this.findOneUserGQL
-    .watch({
-      where: selectOne('', 'email'),
-    })
-    .valueChanges.pipe(
-      map(r => r.data?.findOneUser),
-      shareReplay(1)
-    );
+  constructor(private findManyUserGQL: FindManyUserGQL) {}
 
   users$ = this.findManyUserGQL
     .watch({
       where: {
-        name: { mode: QueryMode.Insensitive },
+        email: {
+          mode: QueryMode.Insensitive,
+          equals: 'Peter@ZenSoftware.ca',
+        },
       },
     })
     .valueChanges.pipe(map(r => r.data?.findManyUser));
 
-  example() {
-    const exampleList = [
-      { id: '1' },
-      { id: '2', ex: 'a' },
-      { id: '3', ex: 'b' },
-      { id: undefined },
-      { id: null },
-      { id: -1 },
-      { id: '' },
-      undefined,
-      null,
-    ];
-
-    console.log(`selectMany(exampleList)`, selectMany(exampleList));
-    console.log(`selectMany(exampleList, 'out', 'ex')`, selectMany(exampleList, 'out', 'ex'));
-    console.log(`selectOne({ id: 7, ex: 'abc' }, 'ex')`, selectOne({ id: 7, ex: 'abc' }, 'ex'));
-  }
+  example() {}
 }
