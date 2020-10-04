2b30d5706d14be853daaa222d1315d71908a26d0
Zen Software <ZenSoftware@users.noreply.github.com>
2020-10-04 15:27:44 -0600

    format

--- apps/api/src/app/graphql/prisma/typeDefs.ts
+++ apps/api/src/app/graphql/prisma/typeDefs.ts
@@ -1,5 +1,6 @@
-import User from './User/typeDefs'
 import { mergeTypeDefs } from '@graphql-tools/merge'
 import { sdlInputs } from '@paljs/plugins'
+
+import User from './User/typeDefs'
 
 export default mergeTypeDefs([sdlInputs(), User])
