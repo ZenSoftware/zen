ac13c2090f3b5c1c07526d9ca053ea9c088c9c49
Zen Software <ZenSoftware@users.noreply.github.com>
2020-12-30 04:19:13 -0700

    environment updated

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -12,14 +12,14 @@ export const environment: EnvironmentBase = {
     port: 5432,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: 'prisma',
+    database: 'zen',
   },
   jwtOptions: {
     privateKey: process.env.JWT_PRIVATE_KEY,
     publicKey: process.env.JWT_PUBLIC_KEY,
     signOptions: {
       algorithm: 'RS256',
-      expiresIn: 3600, // 1 hour (in seconds)
+      expiresIn: 3600, // 1 hour (in seconds). The client will exchange the token every 30 minutes during active sessions
     },
   },
   rememberMeExpiresIn: 2592000, // 30 days (in seconds)
