4f58684ea172151153914613998863473761160b
Zen Software <ZenSoftware@users.noreply.github.com>
2020-12-30 07:23:20 -0700

    removed dead code

--- libs/auth/src/lib/zen-login-page/zen-login-page.component.ts
+++ libs/auth/src/lib/zen-login-page/zen-login-page.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthRegisterGQL } from '@zen/graphql';
 
 @Component({
   selector: 'zen-login-page',
   templateUrl: 'zen-login-page.component.html',
 })
 export class ZenLoginPageComponent {
-  constructor(private router: Router, private authRegisterGQL: AuthRegisterGQL) {}
+  constructor(private router: Router) {}
 
   onLoggedIn() {
     this.router.navigateByUrl('/');
   }
-
-  // createUser() {
-  //   this.authRegisterGQL
-  //     .mutate({
-  //       data: {
-  //         email: 'admin@temp.com',
-  //         password: 'TempTemp',
-  //       },
-  //     })
-  //     .subscribe(({ data }) => console.log('Created', data?.authRegister));
-  // }
 }
