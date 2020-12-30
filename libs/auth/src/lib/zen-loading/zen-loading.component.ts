1af6b67bcde696299cba344012386dff7cc88251
Zen Software <ZenSoftware@users.noreply.github.com>
2020-12-30 11:18:09 -0700

    removed unused component

--- libs/auth/src/lib/zen-loading/zen-loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'zen-loading',
-  templateUrl: 'zen-loading.component.html',
-  styleUrls: ['zen-loading.component.scss'],
-})
-export class ZenLoadingComponent {}
