90d4563c1bbd07e5a1e622bed695781803781128
Zen Software <ZenSoftware@users.noreply.github.com>
2020-12-31 08:27:41 -0700

    fixed spacing

--- apps/api/src/app/auth/gql/gql-user.decorator.ts
+++ apps/api/src/app/auth/gql/gql-user.decorator.ts
@@ -5,8 +5,6 @@ import { RequestUser } from '../request-user';
 
 export const GqlUser = createParamDecorator((data, context: ExecutionContext) => {
   const user = GqlExecutionContext.create(context).getContext().req.user;
-  if (!user) {
-    throw new UnauthorizedException('No user found for request');
-  }
+  if (!user) throw new UnauthorizedException('No user found for request');
   return user as RequestUser;
 });
