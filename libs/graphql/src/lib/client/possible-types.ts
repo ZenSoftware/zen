c544bd8afe1b798ec062c8fd3bd7279a48bb0ec3
Zen Software <ZenSoftware@users.noreply.github.com>
2021-01-03 07:24:41 -0700

    prettier update

--- libs/graphql/src/lib/client/possible-types.ts
+++ libs/graphql/src/lib/client/possible-types.ts
@@ -1,9 +1,11 @@
-export interface PossibleTypesResultData {
-  possibleTypes: {
-    [key: string]: string[];
-  };
-}
-const result: PossibleTypesResultData = {
-  possibleTypes: {},
+
+      export interface PossibleTypesResultData {
+        possibleTypes: {
+          [key: string]: string[]
+        }
+      }
+      const result: PossibleTypesResultData = {
+  "possibleTypes": {}
 };
-export default result;
+      export default result;
+    
