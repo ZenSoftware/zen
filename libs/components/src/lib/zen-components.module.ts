70677c2d1bb8f9f65d5021313661f0dd8c726841
Zen Software <ZenSoftware@users.noreply.github.com>
2021-01-04 15:35:18 -0700

    added zen-errors

--- libs/components/src/lib/zen-components.module.ts
+++ libs/components/src/lib/zen-components.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { ZenConfirmComponent } from './zen-confirm/zen-confirm.component';
+import { ZenErrorsComponent } from './zen-errors/zen-errors.component';
 import { ZenLoadingComponent } from './zen-loading/zen-loading.component';
 import { ZenSnackbarErrorService } from './zen-snackbar-error/zen-snackbar-error.service';
 
@@ -15,10 +17,11 @@ import { ZenSnackbarErrorService } from './zen-snackbar-error/zen-snackbar-error
     ReactiveFormsModule,
     MatButtonModule,
     MatDialogModule,
+    MatInputModule,
     MatProgressSpinnerModule,
   ],
-  declarations: [ZenConfirmComponent, ZenLoadingComponent],
-  exports: [ZenConfirmComponent, ZenLoadingComponent],
+  declarations: [ZenConfirmComponent, ZenErrorsComponent, ZenLoadingComponent],
+  exports: [ZenConfirmComponent, ZenErrorsComponent, ZenLoadingComponent],
   providers: [ZenSnackbarErrorService],
 })
 export class ZenComponentsModule {}
---
+++ libs/components/src/lib/zen-errors/zen-errors.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zen-errors',
+  templateUrl: 'zen-errors.component.html',
+})
+export class ZenErrorsComponent {}
