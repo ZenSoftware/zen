e193cec487d929099cb1d9dc8d72c8675f382f10
Zen Software <ZenSoftware@users.noreply.github.com>
2021-01-05 06:16:27 -0700

    login page animation styling

--- libs/auth/src/lib/zen-login-page/zen-login-page.component.ts
+++ libs/auth/src/lib/zen-login-page/zen-login-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { verticalAccordion } from '../animations';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'zen-login-page',
   templateUrl: 'zen-login-page.component.html',
+  animations: [...verticalAccordion],
 })
 export class ZenLoginPageComponent {
   constructor(private router: Router, private auth: AuthService) {
