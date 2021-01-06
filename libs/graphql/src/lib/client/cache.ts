6cb55e02daed3dc5b4b816572f1741d13f54fa56
Zen Software <ZenSoftware@users.noreply.github.com>
2021-01-05 17:18:07 -0700

    removed duplicate import

--- libs/graphql/src/lib/client/cache.ts
+++ libs/graphql/src/lib/client/cache.ts
@@ -1,5 +1,4 @@
 import { makeVar } from '@apollo/client/core';
 
-export { makeVar } from '@apollo/client/core';
 export const loggedInVar = makeVar(false);
 export const userRolesVar = makeVar<string[]>([]);
