4480fc57f06fcc4128786f7d6c61ac0b465e0731
Zen Software <ZenSoftware@users.noreply.github.com>
2021-02-04 05:47:23 -0700

    export HttpUser

--- apps/api/src/app/auth/index.ts
+++ apps/api/src/app/auth/index.ts
@@ -4,4 +4,5 @@ export * from './gql';
 export { RequestUser } from './request-user';
 export * from './roles.decorator';
 export * from './auth.service';
+export * from './http-user.decorator';
 export { Role } from '@prisma/client';
