f614d3ffea5755df6968cb29ef95990367680519
Zen Software <ZenSoftware@users.noreply.github.com>
2021-02-05 19:13:56 -0700

    added make Prisma helpers in @zen/graphql

---
+++ libs/graphql/src/lib/tools/make-helpers.ts
@@ -0,0 +1,37 @@
+import { trimObjectStrings } from '@zen/common';
+import { omit } from 'lodash-es';
+/**
+ * Omits `__typename` and `id` properties from an object
+ */
+export function makeSetObject(obj: object | undefined) {
+  return omit(obj, ['__typename', 'id']);
+}
+
+/**
+ * Makes a Prisma update object from a value object
+ */
+export function makeUpdateObject(obj: object) {
+  if (!obj) return undefined;
+
+  const update = Object.entries(obj).reduce((accum: { [k: string]: unknown }, [key, val]) => {
+    accum[key] = { set: val };
+    return accum;
+  }, {});
+
+  return { update };
+}
+
+/**
+ * Makes a Prisma create object from a value object
+ */
+export function makeCreateObject(obj: object) {
+  if (!obj) return undefined;
+
+  const create = Object.entries(obj).reduce((accum: { [k: string]: unknown }, [key, val]) => {
+    if (Array.isArray(val)) accum[key] = { set: val };
+    else accum[key] = val;
+    return accum;
+  }, {});
+
+  return { create };
+}
