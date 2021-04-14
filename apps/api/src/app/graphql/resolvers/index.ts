2292f8bee4ba8336dd031c837b901072adefce07
Zen Software <ZenSoftware@users.noreply.github.com>
2021-04-14 09:43:34 -0600

    integrated Upload scalar for GraphQL

--- apps/api/src/app/graphql/resolvers/index.ts
+++ apps/api/src/app/graphql/resolvers/index.ts
@@ -9,7 +9,6 @@ export const NEST_RESOLVERS = [AuthResolver, UserResolver];
 
 export const NEST_TYPE_DEFS = [AuthTypeDef, UserTypeDef].filter(x => x);
 
-export const ALL_TYPE_DEFS = mergeTypeDefs([PRISMA_TYPE_DEFS, ...NEST_TYPE_DEFS]);
-
+export const ALL_TYPE_DEFS = mergeTypeDefs(['scalar Upload', PRISMA_TYPE_DEFS, ...NEST_TYPE_DEFS]);
 export const GRAPHQL_SCHEMA = makeExecutableSchema({ typeDefs: ALL_TYPE_DEFS });
 export const PRISMA_SCHEMA = makeExecutableSchema({ typeDefs: PRISMA_TYPE_DEFS });
--- tools/graphql-codegen/nest-resolvers-index.temp.js
+++ tools/graphql-codegen/nest-resolvers-index.temp.js
@@ -22,7 +22,7 @@ import PRISMA_TYPE_DEFS from '../prisma/typeDefs';\n`;
     .replace(/,/g, ',\n  ');
   indexSource += `\n\nexport const NEST_TYPE_DEFS = [\n  ${bulkTypeDefExportString}\n].filter(x => x);\n\n`;
 
-  indexSource += `export const ALL_TYPE_DEFS = mergeTypeDefs([PRISMA_TYPE_DEFS, ...NEST_TYPE_DEFS]);\n
+  indexSource += `export const ALL_TYPE_DEFS = mergeTypeDefs(['scalar Upload', PRISMA_TYPE_DEFS, ...NEST_TYPE_DEFS]);
 export const GRAPHQL_SCHEMA = makeExecutableSchema({ typeDefs: ALL_TYPE_DEFS });
 export const PRISMA_SCHEMA = makeExecutableSchema({ typeDefs: PRISMA_TYPE_DEFS });\n`;
 
