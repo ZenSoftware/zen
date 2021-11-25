02fa77bbccd05821f755ef5ad11c2179bbbddf47
Zen Software <ZenSoftware@users.noreply.github.com>
2021-11-24 19:39:20 -0800

    replicate RBAC rules of ASP.NET Core

--- apps/api/src/app/auth/gql/gql.guard.ts
+++ apps/api/src/app/auth/gql/gql.guard.ts
@@ -5,6 +5,10 @@ import { AuthGuard } from '@nestjs/passport';
 import { Role } from '@prisma/client';
 
 @Injectable()
+/**
+ * Replicates RBAC rules for [ASP.NET Core](https://docs.microsoft.com/en-us/aspnet/core/security/authorization/roles?view=aspnetcore-6.0).
+ * **Super** users are granted unlimited access.
+ */
 export class GqlGuard extends AuthGuard('jwt') {
   constructor(private readonly reflector: Reflector) {
     super();
@@ -15,19 +19,25 @@ export class GqlGuard extends AuthGuard('jwt') {
 
     const ctx = GqlExecutionContext.create(context);
     const user = ctx.getContext().req.user;
+    const userRoles: Role[] = user.roles ?? [];
 
-    const classRoles = this.reflector.get<Role[]>('roles', ctx.getClass());
-    const handlerRoles = this.reflector.get<Role[]>('roles', ctx.getHandler());
+    // Give super users unlimited access
+    if (userRoles.includes(Role.Super)) return true;
 
-    let allowedRoles: Role[] = [];
-    if (classRoles) allowedRoles = classRoles;
-    if (handlerRoles) allowedRoles = allowedRoles.concat(handlerRoles);
+    let classRoles = this.reflector.get<Role[]>('roles', ctx.getClass());
+    classRoles = classRoles ?? [];
 
-    if (allowedRoles.length === 0) return true;
+    let handlerRoles = this.reflector.get<Role[]>('roles', ctx.getHandler());
+    handlerRoles = handlerRoles ?? [];
 
-    allowedRoles.push(Role.Super); // Grants Super user full access
+    if (classRoles.length > 0) {
+      if (!userRoles.some(r => classRoles.includes(r))) return false;
+      if (handlerRoles.length > 0 && !userRoles.some(r => handlerRoles.includes(r))) return false;
+    } else if (handlerRoles.length > 0 && !userRoles.some(r => handlerRoles.includes(r))) {
+      return false;
+    }
 
-    return user.roles && user.roles.some((userRole: Role) => allowedRoles.includes(userRole));
+    return true;
   }
 
   getRequest(context: ExecutionContext) {
