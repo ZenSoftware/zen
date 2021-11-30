1341d88b82cff81706a321ecd9e78edb38e3bda3
Zen Software <ZenSoftware@users.noreply.github.com>
2021-11-30 14:59:49 -0700

    Requests only send auth token if it is API endpoint

--- libs/common/src/lib/http-request-interceptor.ts
+++ libs/common/src/lib/http-request-interceptor.ts
@@ -3,12 +3,23 @@ import { Injectable } from '@angular/core';
 import ls from 'localstorage-slim';
 import { Observable } from 'rxjs';
 
+import { Environment } from './environment';
+
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  apiHost: string;
+  gqlHost: string;
+
+  constructor(env: Environment) {
+    this.apiHost = new URL(env.url.api).host;
+    this.gqlHost = new URL(env.url.graphql).host;
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = ls.get('token', { decrypt: true }) as string | undefined;
+    const reqHost = new URL(req.url).host;
 
-    if (token) {
+    if (token && (reqHost === this.apiHost || reqHost === this.gqlHost)) {
       const modifiedReq = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`),
       });
