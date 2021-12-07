0371a834fb50492ec237362f32bd99a37749840f
Zen Software <ZenSoftware@users.noreply.github.com>
2021-12-06 20:22:47 -0700

    throw UnauthorizedExceptions

--- apps/api/src/app/auth/auth-logic.ts
+++ apps/api/src/app/auth/auth-logic.ts
@@ -1,3 +1,4 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { Role } from '@prisma/client';
 
 type RoleType = string[] | Role[] | undefined;
@@ -15,10 +16,11 @@ export function authLogic(
   if (_userRoles.includes(Role.Super)) return true;
 
   if (_classRoles.length > 0) {
-    if (!_userRoles.some(r => _classRoles.includes(r as Role))) return false;
-    if (_handlerRoles.length > 0 && !_userRoles.some(r => _handlerRoles.includes(r))) return false;
+    if (!_userRoles.some(r => _classRoles.includes(r as Role))) throw new UnauthorizedException();
+    if (_handlerRoles.length > 0 && !_userRoles.some(r => _handlerRoles.includes(r)))
+      throw new UnauthorizedException();
   } else if (_handlerRoles.length > 0 && !_userRoles.some(r => _handlerRoles.includes(r))) {
-    return false;
+    throw new UnauthorizedException();
   }
 
   return true;
