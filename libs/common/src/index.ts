058139eb6ef72334d5eada7cad3f0701018bcb41
Zen Software <ZenSoftware@users.noreply.github.com>
2021-12-11 04:14:13 -0700

    removed unused validators

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -3,7 +3,5 @@ export * from './lib/http-request-interceptor';
 export * from './lib/is-number';
 export * from './lib/enum-helpers';
 export * from './lib/nameof';
-export * from './lib/email-validator';
-export * from './lib/password-validator';
 export * from './lib/trim-object-strings';
 export * from './lib/token-var';
