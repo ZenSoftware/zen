7811f947a8761100c5fa98d0e243354db6077a75
Zen Software <ZenSoftware@users.noreply.github.com>
2021-12-11 04:04:20 -0700

    format all

--- libs/auth/jest.config.js
+++ libs/auth/jest.config.js
@@ -6,16 +6,16 @@ module.exports = {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-    }
+    },
   },
   coverageDirectory: '../../coverage/libs/auth',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
-  ]
+  ],
 };
