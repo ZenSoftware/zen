273e4d95715cae6775ca27cee68a27e9304a9e4b
Zen Software <ZenSoftware@users.noreply.github.com>
2021-12-20 01:56:58 -0700

    remove unused import

--- apps/api/src/app/auth/jwt.strategy.ts
+++ apps/api/src/app/auth/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request as ExReq } from 'express';
 import { Strategy } from 'passport-jwt';
