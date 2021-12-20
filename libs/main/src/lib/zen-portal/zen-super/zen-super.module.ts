c15e166111411aaf3802089b2389851b83a25bdf
Zen Software <ZenSoftware@users.noreply.github.com>
2021-12-19 23:10:10 -0700

    removed duplicate HttpClientModule

--- libs/main/src/lib/zen-portal/zen-super/zen-super.module.ts
+++ libs/main/src/lib/zen-portal/zen-super/zen-super.module.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ZenComponentsModule } from '@zen/components';
@@ -10,13 +9,7 @@ import { ZenSuperPageComponent } from './zen-super-page/zen-super-page.component
 import { ZenSuperRoutingModule } from './zen-super-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    MatButtonModule,
-    ZenComponentsModule,
-    ZenSuperRoutingModule,
-  ],
+  imports: [CommonModule, MatButtonModule, ZenComponentsModule, ZenSuperRoutingModule],
   declarations: [ZenSuperPageComponent, ZenSampleSubscriptionComponent, ZenSampleUploadComponent],
 })
 export class ZenSuperModule {}
