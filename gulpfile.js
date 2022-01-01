57a4232af3b39918e32227c44768e576b0ead792
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-01 02:45:30 -0700

    clarified tsconfig for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 eval(
   require('typescript').transpile(
     require('fs').readFileSync('./tools/gulpfile.ts').toString(),
-    require('./tools/tsconfig.json').compilerOptions
+    require('./tools/tsconfig.gulp.json').compilerOptions
   )
 );
