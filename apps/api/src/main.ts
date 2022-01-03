b5f072401369ec99284dbe7747f8ebed31f7a41a
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-02 22:51:23 -0700

    moved port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,13 +7,13 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 async function bootstrap() {
-  const port = process.env.PORT || environment.expressPort;
-
   const app = await NestFactory.create(AppModule, { cors: environment.cors });
 
   app.use(graphqlUploadExpress(environment.graphql.uploads));
 
   if (environment.production) app.use(helmet());
+
+  const port = process.env.PORT || environment.expressPort;
 
   await app.listen(port, () => {
     Logger.log(`Listening at http://localhost:${port}`);
