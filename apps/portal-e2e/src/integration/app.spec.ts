19340a5cee4668432c3e5582aefe0aa4f15dbce8
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-23 03:24:44 -0700

    sample test

--- apps/portal-e2e/src/integration/app.spec.ts
+++ apps/portal-e2e/src/integration/app.spec.ts
@@ -1,13 +1,15 @@
-import { getGreeting } from '../support/app.po';
+// import { getGreeting } from '../support/app.po';
 
 describe('portal', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/login'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('me@zen.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to portal!');
+    // getGreeting().contains('Welcome to portal!');
+    cy.get(`[type="submit"]`).contains('Login');
+    cy.get(`[placeholder="Username"]`).type('zen');
   });
 });
