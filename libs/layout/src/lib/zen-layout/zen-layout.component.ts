e4d4e7a7054023f8ba24fa122e3233b70b6ad6b6
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-29 07:26:04 -0700

    destroy subscription on component destructor

--- libs/layout/src/lib/zen-layout/zen-layout.component.ts
+++ libs/layout/src/lib/zen-layout/zen-layout.component.ts
@@ -1,19 +1,20 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Event, NavigationStart, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'zen-layout',
   templateUrl: './zen-layout.component.html',
 })
-export class ZenLayoutComponent {
+export class ZenLayoutComponent implements OnDestroy {
   @ViewChild('drawer') drawer!: MatSidenav;
+  #sub: Subscription;
 
   constructor(private breakpointObserver: BreakpointObserver, router: Router) {
-    router.events.subscribe((event: Event) => {
+    this.#sub = router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
         this.drawer.close();
       }
@@ -24,4 +25,8 @@ export class ZenLayoutComponent {
     map(result => result.matches),
     shareReplay()
   );
+
+  ngOnDestroy() {
+    if (this.#sub) this.#sub.unsubscribe();
+  }
 }
