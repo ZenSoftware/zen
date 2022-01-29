d027905eafda130382384bc9d05c1f9324b9604f
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-29 06:20:17 -0700

    refactored menu items

--- apps/portal/src/app/app.module.ts
+++ apps/portal/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -22,7 +22,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     RouterModule.forRoot([]),
-    MatToolbarModule,
+    MatListModule,
     MainModule,
     ZenAuthModule,
     ZenLayoutModule,
--- libs/layout/src/lib/zen-layout.module.ts
+++ libs/layout/src/lib/zen-layout.module.ts
@@ -3,10 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
 
 import { ZenLayoutComponent } from './zen-layout/zen-layout.component';
 
@@ -16,10 +14,8 @@ import { ZenLayoutComponent } from './zen-layout/zen-layout.component';
     LayoutModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
-    RouterModule,
   ],
   declarations: [ZenLayoutComponent],
   exports: [ZenLayoutComponent],
