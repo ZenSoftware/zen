272041da191db445f71898235f099265ff548b3b
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-29 06:33:34 -0700

    refactored login links

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@zen/auth';
 
 @Component({
   selector: 'zen-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public auth: AuthService) {}
+}
