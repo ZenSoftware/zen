a9197a95ac4dff75d055f03fc66f83612757e664
Zen Software <ZenSoftware@users.noreply.github.com>
2022-02-20 03:35:06 -0700

    removed unused scss

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { AuthService } from '@zen/auth';
 @Component({
   selector: 'zen-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(public auth: AuthService) {}
