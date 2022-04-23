334292b9861541b91aaebdc4293180a8fe8a0726
Zen Software <ZenSoftware@users.noreply.github.com>
2022-04-23 00:45:24 -0600

    nx 14 update

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'api',
-  preset: '../../jest.preset.js',
+
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,4 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/api',
+  preset: '../../jest.preset.ts',
 };
--- libs/graphql/jest.config.ts
+++ libs/graphql/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'main',
-  preset: '../../jest.preset.js',
+  displayName: 'graphql',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/main',
+  coverageDirectory: '../../coverage/libs/graphql',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
