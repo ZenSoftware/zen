6395d67e7d5966696153a80f2672c7b75eecab24
Zen Software <ZenSoftware@users.noreply.github.com>
2022-04-23 18:22:20 -0600

    test registration form

--- libs/auth/src/lib/zen-register-form/zen-register-form.spec.ts
+++ libs/auth/src/lib/zen-register-form/zen-register-form.spec.ts
@@ -5,10 +5,14 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ZenComponentsModule } from '@zen/components';
-import { AuthRegisterGQL } from '@zen/graphql';
+import {
+  AuthRegister,
+  AuthRegisterDocument,
+  AuthRegisterGQL,
+  AuthRegisterVariables,
+} from '@zen/graphql';
 import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
 import { ZenRegisterFormComponent } from './zen-register-form.component';
@@ -30,7 +34,6 @@ describe('ZenRegisterFormComponent', () => {
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
-        MatProgressBarModule,
         ApolloTestingModule,
         ZenComponentsModule,
       ],
@@ -46,7 +49,42 @@ describe('ZenRegisterFormComponent', () => {
   });
 
   it('should compile', () => {
-    controller.verify();
     expect(component).toBeTruthy();
+  });
+
+  it('should register user', done => {
+    component.username.setValue('zen');
+    component.email.setValue('me@zen.ca');
+    component.password.setValue('samplepass');
+    component.passwordConfirm.setValue('samplepass');
+    component.acceptTerms.setValue(true);
+
+    expect(component.form.valid).toEqual(true);
+
+    component.onSubmit();
+    const op = controller.expectOne(AuthRegisterDocument);
+    const variables = op.operation.variables as AuthRegisterVariables;
+
+    expect(variables.data.username).toEqual(component.username.value);
+
+    const data: AuthRegister = {
+      authRegister: {
+        __typename: 'AuthSession',
+        id: 1,
+        expiresIn: 1000,
+        rememberMe: true,
+        roles: ['Super'],
+        token: '1234',
+      },
+    };
+
+    component.registered.subscribe(authSession => {
+      expect(authSession.token).toEqual(data.authRegister.token);
+      done();
+    });
+
+    op.flush({ data });
+
+    controller.verify();
   });
 });
