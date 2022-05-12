c85a9e5ef0b1cc8426f4eb6acab689fd68fe4cb6
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-12 17:09:08 -0600

    chore(package.json): nx v14.1.5 update

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'api',
 
   globals: {
@@ -12,5 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/api',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
