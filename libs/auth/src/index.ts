b47ad697cce0d3f586718f08b0ee4a0d4220963f
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-14 10:18:51 -0600

    refactor(auth): exposed validation pipes

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,5 +1,6 @@
+export * from './lib/auth-interceptor';
 export * from './lib/auth.service';
 export * from './lib/guards';
-export * from './lib/auth-interceptor';
 export * from './lib/token-var';
+export * from './lib/validators';
 export * from './lib/zen-auth.module';
