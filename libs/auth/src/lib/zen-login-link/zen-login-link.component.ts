d0c039de5a38ec9b0183522cc8ea98119423a517
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-15 03:07:44 -0600

    feat(zen-login-link): hide link if already on login page

--- libs/auth/src/lib/zen-login-link/zen-login-link.component.ts
+++ libs/auth/src/lib/zen-login-link/zen-login-link.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable, filter, map } from 'rxjs';
 
 import { AuthService } from '../auth.service';
 
@@ -8,6 +10,15 @@ import { AuthService } from '../auth.service';
 })
 export class ZenLoginLinkComponent {
   @Input() displayLogout = true;
+  displayLogin$: Observable<boolean>;
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {
+    this.displayLogin$ = this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+        if (event.url === '/login') return false;
+        return true;
+      })
+    );
+  }
 }
