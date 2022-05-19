b778021c7a8665305f5f08a3c418081bc1a29ef8
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-18 18:49:18 -0600

    refactor(api/controllers): made test dependent on code

--- apps/api/src/app/controllers/auth.controller.spec.ts
+++ apps/api/src/app/controllers/auth.controller.spec.ts
@@ -1,35 +1,75 @@
-import { URLSearchParams } from 'url';
+import { Test, TestingModule } from '@nestjs/testing';
 
+import { AuthService } from '../auth';
+import { ConfigService } from '../config';
 import { AuthSession } from '../graphql/models';
+import { AuthController } from './auth.controller';
 
 describe('AuthSession query string', () => {
-  const loginConfirmedUrl = 'http://localhost:4200/#/login-confirmed';
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            getAuthSession: () => {
+              return <AuthSession>{
+                id: 1,
+                roles: ['Super', 'Registered'],
+                expiresIn: 123,
+                rememberMe: true,
+                token: 'abc.def_+/ghi.jkl==',
+              };
+            },
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: <ConfigService>{
+            oauth: { loginConfirmedURL: 'http://localhost:4200/#/login-confirmed' },
+          },
+        },
+      ],
+    }).compile();
+
+    await module.resolve(AuthService);
+    controller = module.get(AuthController);
+  });
 
   it('constructs a query string from an AuthSession', () => {
-    const authSession: AuthSession = {
-      id: 1,
-      roles: ['Super', 'Registered'],
-      expiresIn: 123,
-      rememberMe: true,
-      token: 'abc.def_+/ghi.jkl==',
-    };
-
-    const uriEncodedAuthSession = { ...authSession };
-    uriEncodedAuthSession.token = encodeURIComponent(authSession.token);
-
-    const searchParams = new URLSearchParams(Object.entries(uriEncodedAuthSession as object));
-    const redirectUrl = loginConfirmedUrl + '?' + searchParams;
+    const redirectUrl = controller.getLoginConfirmedURL(null);
     expect(redirectUrl).toEqual(
       'http://localhost:4200/#/login-confirmed?id=1&roles=Super%2CRegistered&expiresIn=123&rememberMe=true&token=abc.def_%252B%252Fghi.jkl%253D%253D'
     );
 
-    const reconstructed: AuthSession = {
-      id: parseInt(searchParams.get('id')),
-      expiresIn: parseInt(searchParams.get('expiresIn')),
-      rememberMe: searchParams.get('rememberMe') === 'true' ? true : false,
-      roles: searchParams.get('roles') ? searchParams.get('roles').split(',') : [],
-      token: decodeURIComponent(searchParams.get('token')),
-    };
-    expect(reconstructed).toEqual(authSession);
+    // const authSession: AuthSession = {
+    //   id: 1,
+    //   roles: ['Super', 'Registered'],
+    //   expiresIn: 123,
+    //   rememberMe: true,
+    //   token: 'abc.def_+/ghi.jkl==',
+    // };
+
+    // const uriEncodedAuthSession = { ...authSession };
+    // uriEncodedAuthSession.token = encodeURIComponent(authSession.token);
+
+    // const searchParams = new URLSearchParams(Object.entries(uriEncodedAuthSession as object));
+    // const loginConfirmedUrl = 'http://localhost:4200/#/login-confirmed';
+    // const redirectUrl = loginConfirmedUrl + '?' + searchParams;
+    // expect(redirectUrl).toEqual(
+    //   'http://localhost:4200/#/login-confirmed?id=1&roles=Super%2CRegistered&expiresIn=123&rememberMe=true&token=abc.def_%252B%252Fghi.jkl%253D%253D'
+    // );
+
+    // const reconstructed: AuthSession = {
+    //   id: parseInt(searchParams.get('id')),
+    //   expiresIn: parseInt(searchParams.get('expiresIn')),
+    //   rememberMe: searchParams.get('rememberMe') === 'true' ? true : false,
+    //   roles: searchParams.get('roles') ? searchParams.get('roles').split(',') : [],
+    //   token: decodeURIComponent(searchParams.get('token')),
+    // };
+    // expect(reconstructed).toEqual(authSession);
   });
 });
