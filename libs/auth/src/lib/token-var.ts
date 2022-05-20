29de8bf26d38fb2352c03c95e80ca989ecc1e966
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-19 19:33:03 -0600

    fix(auth): fixed zen-register-form tests

--- libs/auth/src/lib/token-var.ts
+++ libs/auth/src/lib/token-var.ts
@@ -1,5 +1,5 @@
 import { makeVar } from '@apollo/client/core';
 import ls from 'localstorage-slim';
 
-const token: string = ls.get('token', { decrypt: true }) ?? '';
+const token: string = ls?.get('token', { decrypt: true }) ?? '';
 export const tokenVar = makeVar<string | null>(token);
