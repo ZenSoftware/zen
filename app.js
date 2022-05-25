72c67e20ab85a1231786a4d88bb76a377e58623d
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-25 05:03:58 -0600

    feat(electron): OIDC integrated

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const URL = require('url').URL;
+
+let INDEX_PATH;
+if (app.isPackaged) INDEX_PATH = 'index.html';
+else INDEX_PATH = path.join(__dirname, `/dist/apps/electron/index.html`);
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1600,
     height: 900,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js')
-    // }
   });
 
-  win.loadFile(path.join(__dirname, `/dist/apps/electron/index.html`));
+  win.loadFile(INDEX_PATH);
   win.webContents.openDevTools();
+
+  const filter = { urls: ['*://localhost/*'] };
+
+  win.webContents.session.webRequest.onBeforeRequest(filter, (details, callback) => {
+    const requestURL = new URL(details.url);
+
+    if (requestURL.hash.startsWith('#/login-confirmed')) {
+      const loginConfirmedURL = new URL(INDEX_PATH);
+      loginConfirmedURL.protocol = 'file:';
+      loginConfirmedURL.hash = requestURL.hash;
+      win.loadURL(loginConfirmedURL.toString());
+    } else {
+      callback(details);
+    }
+  });
 }
 
 app.whenReady().then(() => {
