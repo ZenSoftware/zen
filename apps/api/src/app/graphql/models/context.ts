ed92c0f70fa43a8dabb84c0a982f0a5f758a4c8c
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-09 00:01:50 -0600

    fix(api): resolved `module has no exports` warning
    undefined

--- apps/api/src/app/graphql/models/context.ts
+++ apps/api/src/app/graphql/models/context.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 import { PrismaService } from '../../prisma';
 
-export interface IContext {
+export class IContext {
   req?: Request;
   res?: Response;
   prisma?: PrismaService;
