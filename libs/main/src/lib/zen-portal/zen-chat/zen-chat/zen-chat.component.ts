99509ae22f5667b86a9d01bb77b02fafe62433ea
Zen Software <ZenSoftware@users.noreply.github.com>
2022-07-02 15:34:33 -0600

    fix(socketio): clean up code for socketio component
    undefined

--- libs/main/src/lib/zen-portal/zen-chat/zen-chat/zen-chat.component.ts
+++ libs/main/src/lib/zen-portal/zen-chat/zen-chat/zen-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { ZenSocketService } from '../zen-socket.service';
 
@@ -6,16 +6,22 @@ import { ZenSocketService } from '../zen-socket.service';
   selector: 'zen-chat',
   templateUrl: 'zen-chat.component.html',
 })
-export class ZenChatComponent implements OnInit {
+export class ZenChatComponent implements OnInit, OnDestroy {
   constructor(private socket: ZenSocketService) {}
 
   ngOnInit(): void {
-    this.socket.on('msgToClient', (args: any) => {
-      console.log('msgToClient:', args);
-    });
+    this.socket.on('msgToClient', this.msgToClient);
   }
+
+  msgToClient = (args: any) => {
+    console.log('msgToClient:', args);
+  };
 
   sendMessage() {
     this.socket.emit('msgToServer', { test: 'hello world' });
+  }
+
+  ngOnDestroy(): void {
+    this.socket.removeListener('msgToClient', this.msgToClient);
   }
 }
