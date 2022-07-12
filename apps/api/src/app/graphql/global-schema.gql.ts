a27c5a47559919d997ea65c3cca09f8a2cd64ff3
Zen Software <ZenSoftware@users.noreply.github.com>
2022-07-12 00:19:16 -0600

    docs(graphql): description of file
    undefined

--- apps/api/src/app/graphql/global-schema.gql.ts
+++ apps/api/src/app/graphql/global-schema.gql.ts
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+/** Place global GraphQL type definitions here */
+
 export const typeDefs = gql`
   scalar Upload # for graphql-upload
   scalar Json # for Prisma Json type
