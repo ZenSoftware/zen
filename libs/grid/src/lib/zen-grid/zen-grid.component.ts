d15b61d70142161ae5b354d731f118df196e5695
Zen Software <ZenSoftware@users.noreply.github.com>
2022-08-16 04:59:17 -0600

    feat(grid): substitute cloneDeep for structuredClone
    undefined

--- libs/grid/src/lib/zen-grid/zen-grid.component.ts
+++ libs/grid/src/lib/zen-grid/zen-grid.component.ts
@@ -33,7 +33,7 @@ import { Action } from '@zen/auth';
 import { ZenConfirmComponent, ZenSnackbarErrorService } from '@zen/components';
 import * as Apollo from 'apollo-angular';
 import { format } from 'date-fns';
-import { cloneDeep, omit } from 'lodash-es';
+import { omit } from 'lodash-es';
 import { Subscription, map } from 'rxjs';
 
 import {
@@ -127,7 +127,7 @@ export class ZenGridComponent<T extends object> implements AfterContentInit, OnD
     this.#settings = value;
     this.#settings.process = this.#settings.process ?? 'remote';
 
-    this.#defaultSettings = cloneDeep(value.defaultSettings);
+    this.#defaultSettings = structuredClone(value.defaultSettings);
     this.#defaultSettings.state = this.#defaultSettings.state ?? ({} as any);
     (<State>this.#defaultSettings.state).skip = (<State>this.#defaultSettings.state).skip ?? 0;
     (<State>this.#defaultSettings.state).take =
@@ -143,7 +143,7 @@ export class ZenGridComponent<T extends object> implements AfterContentInit, OnD
       this.showDelete = row => ability.can(Action.delete, row);
     }
 
-    this.gridSettings = cloneDeep(this.#defaultSettings) as any;
+    this.gridSettings = structuredClone(this.#defaultSettings) as any;
   }
   get settings() {
     return this.#settings as ZenGridSettings<T>;
@@ -287,7 +287,7 @@ export class ZenGridComponent<T extends object> implements AfterContentInit, OnD
   }
 
   resetGrid() {
-    this.gridSettings = cloneDeep(this.#defaultSettings) as any;
+    this.gridSettings = structuredClone(this.#defaultSettings) as any;
     this.collapseAllDetails();
     if (this.settings.process === 'local') this.dataStateChangeHandler();
   }
