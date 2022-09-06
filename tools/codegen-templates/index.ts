0ce5930df30da8c25654937e51392d0760098a75
Zen Software <ZenSoftware@users.noreply.github.com>
2022-09-06 08:52:32 -0600

    refactor(graphql): moved codegen templates
    undefined

--- tools/codegen-templates/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './client-fields.temp';
-export * from './client-queries.temp';
-export * from './nest-casl.temp';
-export * from './nest-resolvers-abac.temp';
-export * from './nest-resolvers-index.temp';
-export * from './nest-resolvers-rbac.temp';
