8d1ceff2ffd3c8f2c8ae3c6a2d99b07cd2c8a6f7
Zen Software <ZenSoftware@users.noreply.github.com>
2022-11-30 10:26:46 -0700

    refactor(portal): removed legacy material module
    undefined

--- apps/portal/src/app/app.module.ts
+++ apps/portal/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
