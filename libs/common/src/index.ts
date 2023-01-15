65f5a1534c973d695f645d2380a4e9d20ac40e4f
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-15 07:30:48 -0700

    refactor(common): removed isStringNumber helper
    undefined

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -4,6 +4,5 @@ export * from './lib/action';
 export * from './lib/assert-type';
 export * from './lib/enum-helper';
 export * from './lib/environment';
-export * from './lib/is-string-number';
 export * from './lib/role';
 export * from './lib/trim-object-strings';
