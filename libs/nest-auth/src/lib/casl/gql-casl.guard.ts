c0fdac92f63a114ad3583175bec72eb84d05f77b
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-15 07:38:03 -0700

    refactor(auth): refactored metadata keys to be consistent
    undefined

--- libs/nest-auth/src/lib/casl/gql-casl.guard.ts
+++ libs/nest-auth/src/lib/casl/gql-casl.guard.ts
@@ -7,7 +7,7 @@ import { Action } from '@zen/common';
 
 import { ALLOW_ANONYMOUS_KEY } from '../decorators/allow-anonymous.decorator';
 import { CASL_FACTORY_TOKEN } from './casl-factory.token';
-import { SUBJECT_KEY } from './casl-subject.decorator';
+import { CASL_SUBJECT_KEY } from './casl-subject.decorator';
 
 export function GqlCaslGuard(...actions: Array<Action>) {
   @Injectable()
@@ -35,8 +35,8 @@ export function GqlCaslGuard(...actions: Array<Action>) {
       await super.canActivate(context);
       const user = ctx.getContext().req.user;
 
-      const classSubject = this.reflector.get<string>(SUBJECT_KEY, ctx.getClass());
-      const handlerSubject = this.reflector.get<string>(SUBJECT_KEY, ctx.getHandler());
+      const classSubject = this.reflector.get<string>(CASL_SUBJECT_KEY, ctx.getClass());
+      const handlerSubject = this.reflector.get<string>(CASL_SUBJECT_KEY, ctx.getHandler());
       const subjectName = handlerSubject ? handlerSubject : classSubject;
 
       const ability = await this.caslFactory.createAbility(user);
--- libs/nest-auth/src/lib/casl/http-casl.guard.ts
+++ libs/nest-auth/src/lib/casl/http-casl.guard.ts
@@ -5,7 +5,7 @@ import { Action } from '@zen/common';
 
 import { ALLOW_ANONYMOUS_KEY } from '../decorators/allow-anonymous.decorator';
 import { CASL_FACTORY_TOKEN } from './casl-factory.token';
-import { SUBJECT_KEY } from './casl-subject.decorator';
+import { CASL_SUBJECT_KEY } from './casl-subject.decorator';
 
 export function HttpCaslGuard(...actions: Array<Action>) {
   @Injectable()
@@ -31,8 +31,8 @@ export function HttpCaslGuard(...actions: Array<Action>) {
       await super.canActivate(context);
       const { user } = context.switchToHttp().getRequest();
 
-      const classSubject = this.reflector.get<string>(SUBJECT_KEY, context.getClass());
-      const handlerSubject = this.reflector.get<string>(SUBJECT_KEY, context.getHandler());
+      const classSubject = this.reflector.get<string>(CASL_SUBJECT_KEY, context.getClass());
+      const handlerSubject = this.reflector.get<string>(CASL_SUBJECT_KEY, context.getHandler());
       const subjectName = handlerSubject ? handlerSubject : classSubject;
 
       const ability = await this.caslFactory.createAbility(user);
