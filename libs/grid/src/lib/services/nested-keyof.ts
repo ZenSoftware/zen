b2514007a741dfdbe938ac6ded77c5571e60618b
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-17 06:53:30 -0700

    docs(grid): fixed url link to utility function
    undefined

--- libs/grid/src/lib/services/nested-keyof.ts
+++ libs/grid/src/lib/services/nested-keyof.ts
@@ -1,5 +1,5 @@
 /**
- * TypeScript Utility keyof nested object: [blog post](https://dev.to/pffigueiredo/typescript-utility-nested-keyof-object-2pa3)
+ * TypeScript Utility keyof nested object: [blog post](https://dev.to/pffigueiredo/typescript-utility-keyof-nested-object-2pa3)
  */
 export type NestedKeyOf<T extends object> = {
   [Key in keyof T & (string | number)]: T[Key] extends object
