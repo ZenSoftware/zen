ea5a116b8625b3fdd0f7c32e342cfc63c442fe4a
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-19 06:05:59 -0700

    feat(graphql): don't include test files in generation
    undefined

--- tools/zen-generator.ts
+++ tools/zen-generator.ts
@@ -96,7 +96,12 @@ export class ZenGenerator {
 
     // Get the data type names via the filename of the "resolvers" directory
     let dataTypeNames = (await readdir(nestResolversPath))
-      .filter(f => path.basename(f) !== 'index.ts')
+      .filter(
+        f =>
+          path.basename(f) !== 'index.ts' &&
+          !path.basename(f).endsWith('.spec.ts') &&
+          !path.basename(f).endsWith('.test.ts')
+      )
       .map(f => path.basename(f, '.ts')); // Remove ".ts" extension from all names
 
     const indexPath = path.join(nestResolversPath, 'index.ts');
