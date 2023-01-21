e402617499617eabfc083baef0e41319ef525c21
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-20 23:35:01 -0700

    feat(auth): simplified rbac
    undefined

--- libs/nest-auth/src/index.ts
+++ libs/nest-auth/src/index.ts
@@ -1,9 +1,9 @@
 export { AllowAnonymous } from './lib/decorators/allow-anonymous.decorator';
 export { CaslSubject } from './lib/decorators/casl-subject.decorator';
 export { RequestUser } from './lib/models/request-user';
+export { RolesGuard } from './lib/guards/roles.guard';
 export * from './lib/casl-factory';
 export * from './lib/decorators/current-user.decorator';
 export * from './lib/guards/casl.guard';
-export * from './lib/guards/roles.guard';
 export * from './lib/models/jwt-payload';
 export * from './lib/nest-auth.module';
