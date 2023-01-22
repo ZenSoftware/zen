f66bbc4e2c7a9eb95c012bca45b9bc2083cdfc03
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-22 00:32:27 -0700

    docs(deps): directly link to GitHub comment
    undefined

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,6 @@ module.exports = {
   singleQuote: true,
   printWidth: 100,
   arrowParens: 'avoid',
-  // Fix for plugins not working with pnpm https://github.com/trivago/prettier-plugin-sort-imports/issues/51
+  // Fix for plugins not working with pnpm https://github.com/trivago/prettier-plugin-sort-imports/issues/51#issuecomment-1018985805
   plugins: [require.resolve('prettier-plugin-import-sort')],
 };
