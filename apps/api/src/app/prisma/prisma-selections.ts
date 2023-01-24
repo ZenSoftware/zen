fbe1b6172c49edf32d936db94ebcb96d996f84f4
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-23 17:13:57 -0700

    fix(auth): narrow prisma select types to boolean only
    undefined

--- apps/api/src/app/prisma/prisma-selections.ts
+++ apps/api/src/app/prisma/prisma-selections.ts
@@ -1,6 +1,10 @@
 // This file is generated automatically. Do not edit it manually.
 import { Prisma } from './generated';
 
+type BoolOnly<T> = {
+  [P in keyof T]: T[P] extends boolean ? boolean : never;
+};
+
 export type PrismaSelections = {
-  User?: Prisma.UserSelect;
+  User?: BoolOnly<Prisma.UserSelect>;
 };
--- tools/templates/prisma-selections.ts
+++ tools/templates/prisma-selections.ts
@@ -1,11 +1,15 @@
 export function PrismaSelectionsTemplate(names: string[]) {
   let accum = `// This file is generated automatically. Do not edit it manually.
-import { Prisma } from './generated';\n\n`;
+import { Prisma } from './generated';
 
-  accum += `export type PrismaSelections = {\n`;
+type BoolOnly<T> = {
+  [P in keyof T]: T[P] extends boolean ? boolean : never;
+};
+
+export type PrismaSelections = {\n`;
 
   for (const name of names) {
-    accum += `  ${name}?: Prisma.${name}Select;\n`;
+    accum += `  ${name}?: BoolOnly<Prisma.${name}Select>;\n`;
   }
 
   accum += `};\n`;
