179c3045a17d2811a73d4b2a600187ca02482cc7
Zen Software <ZenSoftware@users.noreply.github.com>
2023-01-28 10:55:06 -0700

    fix(graphql): explicitly type array of typedefs
    undefined

--- apps/api/src/app/graphql/resolvers/index.ts
+++ apps/api/src/app/graphql/resolvers/index.ts
@@ -1,5 +1,6 @@
 // This file is generated automatically. Do not edit it manually.
 import { mergeTypeDefs } from '@graphql-tools/merge';
+import { DocumentNode } from 'graphql';
 
 import { typeDefs as GlobalTypeDefs } from '../global-schema.gql';
 import PALJS_TYPE_DEFS from '../paljs/typeDefs';
@@ -9,6 +10,8 @@ import { UserResolver, typeDefs as UserTypeDefs } from './User';
 
 export const NEST_RESOLVERS = [AuthResolver, SampleResolver, UserResolver];
 
-export const NEST_TYPE_DEFS = [AuthTypeDefs, SampleTypeDefs, UserTypeDefs].filter(x => x);
+export const NEST_TYPE_DEFS = [AuthTypeDefs, SampleTypeDefs, UserTypeDefs].filter(
+  x => x
+) as DocumentNode[];
 
 export const ALL_TYPE_DEFS = mergeTypeDefs([GlobalTypeDefs, PALJS_TYPE_DEFS, ...NEST_TYPE_DEFS]);
