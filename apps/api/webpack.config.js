4ef609e8191026b58c74c2928c1f271e7ee930d9
Zen Software <ZenSoftware@users.noreply.github.com>
2023-02-14 08:22:12 -0700

    build(api): source-map-loader rule to skip generated files
    undefined

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,8 +1,30 @@
 const { composePlugins, withNx } = require('@nrwl/webpack');
 
-// Nx plugins for webpack.
-module.exports = composePlugins(withNx(), config => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+module.exports = composePlugins(withNx(), (config, { options, context }) => {
+  /**
+   * The generated Prisma client does not produce source maps, so we need to
+   * filter out the source map loader for the generated client.
+   */
+  const sourceMapLoaderRule = config.module.rules.find(
+    c => c.loader && c.loader.includes('source-map-loader')
+  );
+
+  delete sourceMapLoaderRule.loader;
+
+  sourceMapLoaderRule.use = [
+    {
+      loader: 'source-map-loader',
+      options: {
+        filterSourceMappingUrl: (url, resourcePath) => {
+          if (/generated/.test(resourcePath)) {
+            return 'skip';
+          }
+
+          return true;
+        },
+      },
+    },
+  ];
+
   return config;
 });
