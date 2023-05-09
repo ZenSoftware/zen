bdf387d7b9e4dedac171f6f6295a6fe3216eb2dc
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-09 13:38:36 -0600

    refactor(auth): removed unused jti field
    undefined

--- libs/nest-auth/src/lib/models/jwt-payload.ts
+++ libs/nest-auth/src/lib/models/jwt-payload.ts
@@ -1,7 +1,6 @@
 import { RequestUser } from './request-user';
 
 export interface JwtPayload {
-  readonly jti: string;
   readonly aud: string;
   readonly sub: RequestUser['id'];
   readonly roles: RequestUser['roles'];
