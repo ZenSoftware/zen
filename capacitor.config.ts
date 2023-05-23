1e82f8554a8f6544a0b2d5fdc6aa56111b613cd6
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-23 14:26:05 -0600

    feat(capacitor): added @capacitor/android & @capacitor/ios
    undefined

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'zen',
+  webDir: 'dist/apps/portal',
+  bundledWebRuntime: false,
+};
+
+export default config;
