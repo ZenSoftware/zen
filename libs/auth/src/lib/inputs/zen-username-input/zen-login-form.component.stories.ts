dd34f7fcc998a3186c48070491a7a9b8076a71a4
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-25 20:39:57 -0600

    feat(auth): added story
    undefined

---
+++ libs/auth/src/lib/inputs/zen-username-input/zen-login-form.component.stories.ts
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/angular';
+
+import { ZenUsernameInputComponent } from './zen-username-input.component';
+
+export default {
+  title: 'ZenUsernameInputComponent',
+  component: ZenUsernameInputComponent,
+} as Meta<ZenUsernameInputComponent>;
+
+export const Primary = {
+  render: (args: ZenUsernameInputComponent) => ({
+    props: args,
+  }),
+  args: {
+    required: true,
+  },
+};
