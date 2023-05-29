ef2c7c048ee5060fd977da924e6215299a3c0b81
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-29 17:38:46 -0600

    feat(auth): added stories
    undefined

---
+++ libs/auth/src/lib/inputs/zen-email-input/zen-email-input.stories.ts
@@ -0,0 +1,15 @@
+import { Meta } from '@storybook/angular';
+
+import { ZenEmailInputComponent } from './zen-email-input.component';
+
+export default {
+  title: 'ZenEmailInputComponent',
+  component: ZenEmailInputComponent,
+} as Meta<ZenEmailInputComponent>;
+
+export const Primary = {
+  render: (args: ZenEmailInputComponent) => ({
+    props: args,
+  }),
+  args: {},
+};
