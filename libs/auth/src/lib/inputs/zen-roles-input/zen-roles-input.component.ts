94a2746e79b2a1319f07c8fa49098c7b86b7e696
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-30 22:10:53 -0600

    feat(auth): implemented ZenRolesInputComponent
    undefined

---
+++ libs/auth/src/lib/inputs/zen-roles-input/zen-roles-input.component.ts
@@ -0,0 +1,105 @@
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { NgFor } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material/autocomplete';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { Role } from '@zen/common';
+import { ZenSnackbarError, ZenSnackbarModule } from '@zen/components';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'zen-roles-input',
+  templateUrl: 'zen-roles-input.component.html',
+  standalone: true,
+  imports: [
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    NgFor,
+    ReactiveFormsModule,
+    ZenSnackbarModule,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ZenRolesInputComponent,
+      multi: true,
+    },
+  ],
+})
+export class ZenRolesInputComponent implements ControlValueAccessor, OnDestroy {
+  readonly ROLES = Object.values(Role);
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  readonly control = new FormControl<string[]>([], { nonNullable: true });
+  readonly #subs: Subscription[] = [];
+  touchedListeners: Array<() => unknown> = [];
+
+  constructor(private zenSnackbarError: ZenSnackbarError) {}
+
+  get filteredRoles() {
+    return this.ROLES.filter(r => !this.control.value.includes(r));
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (this.control.value.includes(value)) {
+      this.zenSnackbarError.open(null, {
+        message: `Already has role ${value}`,
+        log: false,
+        copyable: true,
+      });
+    } else if (value) {
+      this.control.setValue([...this.control.value, value]);
+    }
+
+    event.chipInput!.clear();
+  }
+
+  remove(role: string): void {
+    this.control.setValue(this.control.value.filter(r => r !== role));
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.control.setValue([...this.control.value, event.option.viewValue]);
+  }
+
+  writeValue(value: string[]) {
+    if (value) this.control.setValue(value);
+    else this.control.reset();
+  }
+
+  registerOnChange(fn: (_: any) => void) {
+    const sub = this.control.valueChanges.subscribe(fn);
+    this.#subs.push(sub);
+  }
+
+  emitTouched() {
+    this.touchedListeners.forEach(fn => fn());
+  }
+
+  registerOnTouched(fn: any) {
+    this.touchedListeners.push(fn);
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    if (isDisabled) this.control.disable();
+    else this.control.enable();
+  }
+
+  ngOnDestroy() {
+    this.#subs.forEach(s => s.unsubscribe());
+  }
+}
