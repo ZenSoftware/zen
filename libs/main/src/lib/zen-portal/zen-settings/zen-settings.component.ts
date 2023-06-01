f420da4738051a0611d47a62ff35a580fe1c673d
Zen Software <ZenSoftware@users.noreply.github.com>
2023-06-01 00:35:59 -0600

    refactor(auth): zen-account-info component integrated
    undefined

--- libs/main/src/lib/zen-portal/zen-settings/zen-settings.component.ts
+++ libs/main/src/lib/zen-portal/zen-settings/zen-settings.component.ts
@@ -2,12 +2,13 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '@zen/auth';
 import { ZenPasswordChangeComponent } from '@zen/auth';
+import { ZenAccountInfoComponent } from '@zen/auth';
 
 @Component({
   selector: 'zen-settings',
   templateUrl: 'zen-settings.component.html',
   standalone: true,
-  imports: [AsyncPipe, NgIf, ZenPasswordChangeComponent],
+  imports: [AsyncPipe, NgIf, ZenAccountInfoComponent, ZenPasswordChangeComponent],
 })
 export class ZenSettingsComponent {
   constructor(public auth: AuthService) {}
