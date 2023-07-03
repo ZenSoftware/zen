44bcb2275804797ad23a952a8c147c602f2f3885
Zen Software <ZenSoftware@users.noreply.github.com>
2023-07-02 18:59:39 -0600

    feat(babylon): leave room on component destruction
    undefined

--- libs/main/src/lib/zen-portal/zen-babylon/zen-babylon.component.ts
+++ libs/main/src/lib/zen-portal/zen-babylon/zen-babylon.component.ts
@@ -14,7 +14,7 @@ import { Color3, Vector3 } from '@babylonjs/core/Maths/math';
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
 import { Scene } from '@babylonjs/core/scene';
 import { token } from '@zen/auth';
-import { Client } from 'colyseus.js';
+import { Client, Room } from 'colyseus.js';
 import { Subscription, debounce, fromEvent, interval } from 'rxjs';
 
 @Component({
@@ -30,6 +30,7 @@ export class ZenBabylonComponent implements AfterViewInit, OnDestroy {
   playerEntities: Record<string, any> = {};
   playerNextPosition: Record<string, any> = {};
   engine!: Engine;
+  room!: Room;
 
   constructor(private ngZone: NgZone) {}
 
@@ -44,9 +45,9 @@ export class ZenBabylonComponent implements AfterViewInit, OnDestroy {
 
     setTimeout(() => this.updateDimensions());
 
-    this.ngZone.runOutsideAngular(() => {
+    this.ngZone.runOutsideAngular(async () => {
       this.engine = new Engine(this.canvasElement.nativeElement);
-      const scene = this.createScene(this.engine);
+      const scene = await this.createScene(this.engine);
 
       this.engine.runRenderLoop(() => {
         scene.render();
@@ -54,7 +55,7 @@ export class ZenBabylonComponent implements AfterViewInit, OnDestroy {
     });
   }
 
-  createScene(engine: any) {
+  async createScene(engine: any) {
     const scene = new Scene(engine);
 
     const camera = new ArcRotateCamera('camera', Math.PI / 2, 1.0, 550, Vector3.Zero(), scene);
@@ -69,78 +70,77 @@ export class ZenBabylonComponent implements AfterViewInit, OnDestroy {
 
     const colyseusSDK = new Client('ws://localhost:7080');
 
-    colyseusSDK.joinOrCreate<any>('MainRoom', { token: token() }).then(room => {
-      console.log(`Connected to roomId: ${room.roomId}`);
+    this.room = await colyseusSDK.joinOrCreate<any>('MainRoom', { token: token() });
+    console.log(`Connected to roomId: ${this.room.roomId}`);
 
-      room.onStateChange((state: any) => {
-        console.log(`${room.name} new state:`, state.players.values());
-      });
-
-      room.state.players.onAdd((player: any, sessionId: string) => {
-        console.log('player added', player);
-
-        const isCurrentPlayer = sessionId === room.sessionId;
-
-        // create player Sphere
-        const sphere = MeshBuilder.CreateSphere(`player-${sessionId}`, {
-          segments: 8,
-          diameter: 40,
-        });
-
-        // set player spawning position
-        sphere.position.set(player.x, player.y, player.z);
-
-        sphere.material = new StandardMaterial(`player-material-${sessionId}`);
-
-        if (isCurrentPlayer) {
-          (<StandardMaterial>sphere.material).emissiveColor = Color3.FromHexString('#ff9900');
-        } else {
-          (<StandardMaterial>sphere.material).emissiveColor = Color3.Gray();
-        }
-
-        this.playerEntities[sessionId] = sphere;
-      });
-
-      room.state.players.onRemove((player: any, sessionId: string) => {
-        this.playerEntities[sessionId].dispose();
-        delete this.playerEntities[sessionId];
-        delete this.playerNextPosition[sessionId];
-      });
-
-      scene.onPointerDown = (event, pointer) => {
-        if (event.button == 0) {
-          // const targetPosition = (<Vector3>pointer.pickedPoint).clone();
-
-          // // Position adjustments for the current play ground.
-          // targetPosition.y = -1;
-          // if (targetPosition.x > 245) targetPosition.x = 245;
-          // else if (targetPosition.x < -245) targetPosition.x = -245;
-          // if (targetPosition.z > 245) targetPosition.z = 245;
-          // else if (targetPosition.z < -245) targetPosition.z = -245;
-
-          // // set current player's next position immediatelly
-          // this.playerNextPosition[room.sessionId] = targetPosition;
-
-          // // Send position update to the server
-          // room.send('updatePosition', {
-          //   x: targetPosition.x,
-          //   y: targetPosition.y,
-          //   z: targetPosition.z,
-          // });
-
-          room.send('updatePosition', { x: 1, y: 2, z: 3 });
-          console.log('updatePosition');
-        }
-      };
-
-      // scene.registerBeforeRender(() => {
-      //   for (const sessionId in this.playerEntities) {
-      //     const entity = this.playerEntities[sessionId];
-      //     const targetPosition = this.playerNextPosition[sessionId];
-      //     entity.position = Vector3.Lerp(entity.position, targetPosition, 0.05);
-      //   }
-      // });
+    this.room.onStateChange((state: any) => {
+      console.log(`${this.room.name} new state:`, state.players.values());
     });
+
+    this.room.state.players.onAdd((player: any, sessionId: string) => {
+      console.log('player added', player);
+
+      const isCurrentPlayer = sessionId === this.room.sessionId;
+
+      // create player Sphere
+      const sphere = MeshBuilder.CreateSphere(`player-${sessionId}`, {
+        segments: 8,
+        diameter: 40,
+      });
+
+      // set player spawning position
+      sphere.position.set(player.x, player.y, player.z);
+
+      sphere.material = new StandardMaterial(`player-material-${sessionId}`);
+
+      if (isCurrentPlayer) {
+        (<StandardMaterial>sphere.material).emissiveColor = Color3.FromHexString('#ff9900');
+      } else {
+        (<StandardMaterial>sphere.material).emissiveColor = Color3.Gray();
+      }
+
+      this.playerEntities[sessionId] = sphere;
+    });
+
+    this.room.state.players.onRemove((player: any, sessionId: string) => {
+      this.playerEntities[sessionId].dispose();
+      delete this.playerEntities[sessionId];
+      delete this.playerNextPosition[sessionId];
+    });
+
+    scene.onPointerDown = (event, pointer) => {
+      if (event.button == 0) {
+        // const targetPosition = (<Vector3>pointer.pickedPoint).clone();
+
+        // // Position adjustments for the current play ground.
+        // targetPosition.y = -1;
+        // if (targetPosition.x > 245) targetPosition.x = 245;
+        // else if (targetPosition.x < -245) targetPosition.x = -245;
+        // if (targetPosition.z > 245) targetPosition.z = 245;
+        // else if (targetPosition.z < -245) targetPosition.z = -245;
+
+        // // set current player's next position immediatelly
+        // this.playerNextPosition[this.room.sessionId] = targetPosition;
+
+        // // Send position update to the server
+        // this.room.send('updatePosition', {
+        //   x: targetPosition.x,
+        //   y: targetPosition.y,
+        //   z: targetPosition.z,
+        // });
+
+        this.room.send('updatePosition', { x: 1, y: 2, z: 3 });
+        console.log('updatePosition');
+      }
+    };
+
+    // scene.registerBeforeRender(() => {
+    //   for (const sessionId in this.playerEntities) {
+    //     const entity = this.playerEntities[sessionId];
+    //     const targetPosition = this.playerNextPosition[sessionId];
+    //     entity.position = Vector3.Lerp(entity.position, targetPosition, 0.05);
+    //   }
+    // });
 
     return scene;
   }
@@ -157,5 +157,6 @@ export class ZenBabylonComponent implements AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     this.#subs.forEach(sub => sub.unsubscribe());
+    this.room?.leave();
   }
 }
