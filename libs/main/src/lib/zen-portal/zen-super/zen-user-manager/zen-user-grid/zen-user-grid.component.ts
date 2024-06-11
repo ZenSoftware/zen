b1fbb5ef304e99d5c9d8d8da922c94606c9d7663
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-11 07:38:04 -0700

    fix(grid): fixed id column breaking
    undefined

--- libs/main/src/lib/zen-portal/zen-super/zen-user-manager/zen-user-grid/zen-user-grid.component.ts
+++ libs/main/src/lib/zen-portal/zen-super/zen-user-manager/zen-user-grid/zen-user-grid.component.ts
@@ -19,7 +19,6 @@ const DEFAULT_SETTINGS: KendoGridSettings<UserFields> = {
       field: 'id',
       title: 'ID',
       /** @comment set this to the type of id defined in the schema.prisma file */
-      filter: 'string',
       hidden: true,
     },
     {
