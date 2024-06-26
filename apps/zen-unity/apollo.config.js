a1d20dd0f23f1d97564246917019c7735e62b9c8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-25 17:44:24 -0700

    chore(graphql): changed service to unique name
    undefined

--- apps/zen-unity/apollo.config.js
+++ apps/zen-unity/apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   client: {
     service: {
-      name: 'zen',
+      name: 'zen-unity',
       url: 'http://localhost:7080/graphql',
     },
     includes: ['Assets/Scripts/GraphQL/**/*.graphql'],
